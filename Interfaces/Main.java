c6e069aa03c46e9321afefa7d8c5bbd13f8f8041
hemanth5544 <rachapalli.hemanth5544@gmail.com>
2023-08-28 16:41:03 +0530

    updated interfaces

---
+++ Interfaces/Main.java
@@ -0,0 +1,9 @@
+package Interfaces;
+//mutpiel poly is taken here by using interfaces
+public class Main {
+    public static void main(String[] args) {
+        Car car =new Car();
+        car.acc();
+
+    }
+}
